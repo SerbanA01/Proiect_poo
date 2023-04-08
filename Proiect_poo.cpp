26b22d7510ef2d4edead7c9b6bde7ef89cd57d25
SerbanA01 <andrei.serban2@s.unibuc.ro>
2023-04-08 11:03:21 +0300

    Modificare clasa camion

--- Proiect_poo.cpp
+++ Proiect_poo.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <cstring>
+#define _CRT_SECURE_NO_WARNINGS
 using namespace std;
 
 class camion {
-    char nr_inmatriculare;
+    char* nr_inmatriculare;
     double greutate;
     bool activ;
     double masa_net;
+public:
+    int greutate_incarcatura() {
+        if (activ == 1)
+            return masa_net - greutate;
+        else cout << "Camionul nu este activ!";
+        return 0;
+    }
+    camion(const char* numar, double gr_camion) {
+        nr_inmatriculare = new char[strlen(numar) + 1]; 
+        strcpy_s(nr_inmatriculare, strlen(numar) + 1, numar); 
+        greutate = gr_camion;
+        activ = false;
+        masa_net = greutate;
+    }
 
+    void cursa_activa(double incarcatura) {
+        activ = 1;
+        masa_net = incarcatura;
+    }
+    void get_gr() {
+        cout << greutate;
+    }
+    ~camion() {
+        delete[] nr_inmatriculare;
+    }
 };
 class sef {
     char nume, prenume;
@@ -45,7 +71,8 @@ class director {
 
 int main()
 {
-   
+    camion c1("B123DLS", 5000.1);
+    c1.get_gr();
     return 0;
 }
 
